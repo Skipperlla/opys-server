c5783af370a48e434f19434aa177f7e91e0b19a6
Skipperlla <oesmer437@gmail.com>
2022-05-21 18:07:41 +0300

    m

--- validations/Auth.js
+++ validations/Auth.js
@@ -17,27 +17,32 @@ const RegisterValidation = Joi.object({
   email: Joi.string().required().trim().email().messages({
     "any.required": "E-mail alanı boş olamaz.",
     "string.email": "Geçerli bir email adresi giriniz.",
+    "string.empty": "E-mail alanı boş olamaz.",
   }),
   password: Joi.string().required().trim().min(6).max(30).messages({
     "any.required": "Şifre alanı boş olamaz.",
     "string.min": "Şifre en az 6 karakter olmalıdır.",
     "string.max": "Şifre en fazla 30 karakter olmalıdır.",
+    "string.empty": "Şifre alanı boş olamaz.",
   }),
   name: Joi.string().required().trim().min(3).max(30).messages({
-    "any.required": "Ad alanı boş olamaz.",
+    "any.required": "İsim alanı boş olamaz.",
     "string.min": "İsim en az 3 karakter olmalıdır.",
     "string.max": "İsim en fazla 30 karakter olmalıdır.",
+    "string.empty": "İsim alanı boş olamaz.",
   }),
   surname: Joi.string().required().trim().min(3).max(30).messages({
     "any.required": "Soyad alanı boş olamaz.",
-    "string.min": "İsim en az 3 karakter olmalıdır.",
-    "string.max": "İsim en fazla 30 karakter olmalıdır.",
+    "string.min": "Soyad en az 3 karakter olmalıdır.",
+    "string.max": "Soyad en fazla 30 karakter olmalıdır.",
+    "string.empty": "Soyad alanı boş olamaz.",
   }),
   profilePic: Joi.string().messages({
     "string.empty": "İsim alanı boş olamaz.",
   }),
   role: Joi.string().required().trim().messages({
     "any.required": "Rol alanı boş olamaz.",
+    "string.empty": "Rol alanı boş olamaz.",
   }),
 });
 const ResetPassword = Joi.object({
